2c503bce610a3e6493861e1d296f326fc9cacf6c
sukiyaki521 <yamaneyuto922@gmail.com>
2025-05-15 00:38:45 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,7 @@
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+
 def evaluate_accuracy():
     model = joblib.load("model.joblib")
     X, y = load_iris(return_X_y=True)
